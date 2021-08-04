2c68a5f59cd9acac9e70d0d3dd8a095eba732746
hokarith <leonidas51000@hotmail.com>
2021-08-04 18:15:28 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgramStructure
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Stack();
+
+            st.Push(1);
+            st.Push(10);
+            st.Push(100);
+
+            System.Console.WriteLine(st.Pop());
+            System.Console.WriteLine(st.Pop());
+            System.Console.WriteLine(st.Pop());
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProgramStructure
+{
+    public class Stack{
+        
+        Position first;
+
+        public void Push(object item){
+            first = new Position(first, item);
+        }
+
+        public object Pop(){
+            if(first == null)   throw new InvalidOperationException();
+
+            object result = first.item;
+            first = first.next;
+            return result;
+        }
+    }
+
+    class Position{
+
+        public Position next;
+
+        public object item;
+
+        public Position(Position next, object item){
+            this.next = next;
+            this.item = item;
+        } 
+    }
+}
